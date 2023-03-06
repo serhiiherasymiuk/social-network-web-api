5fc020ecb33cea7c1f3ec0f4a5bba46475ae64b4
Админ <@DESKTOP-8DFR7S5>
2023-03-06 20:10:51 +0200

    Add OpenAIController

---
+++ WebAPI/Controllers/OpenAIController.cs
@@ -0,0 +1,18 @@
+﻿using BusinessLogic.Interfaces;
+using BusinessLogic.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class OpenAIController : ControllerBase
+    {
+        IOpenAIService openAIService;
+        public OpenAIController(IOpenAIService openAIService)
+        {
+            this.openAIService = openAIService;
+        }
+    }
+}
