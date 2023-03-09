d75fba0b367dc5eb70a9ca9154f4112c3be2ecad
Админ <@DESKTOP-8DFR7S5>
2023-03-09 20:07:13 +0200

    Add GetAll method

--- WebAPI/Controllers/UsersController.cs
+++ WebAPI/Controllers/UsersController.cs
@@ -20,34 +20,5 @@ namespace WebAPI.Controllers
         {
             return Ok(await usersService.GetAll());
         }
-
-        [HttpGet("{id}")]
-        public async Task<IActionResult> Get([FromRoute] int id)
-        {
-            var item = await usersService.GetById(id);
-            if (item == null) return NotFound();
-            return Ok(item);
-        }
-
-        [HttpPost]
-        public async Task<IActionResult> Create([FromBody] User user)
-        {
-            await usersService.Create(user);
-            return Ok();
-        }
-
-        [HttpPut]
-        public async Task<IActionResult> Edit([FromBody] User user)
-        {
-            await usersService.Edit(user);
-            return Ok();
-        }
-
-        [HttpDelete("{id}")]
-        public async Task<IActionResult> Delete([FromRoute] int id)
-        {
-            await usersService.Delete(id);
-            return Ok();
-        }
     }
 }
