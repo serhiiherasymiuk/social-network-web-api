ad001f7be576db0f14f8cd6b675bee9fe772b3b0
Админ <@DESKTOP-8DFR7S5>
2023-03-13 17:43:20 +0200

    Add MessageDTO

---
+++ BusinessLogic/DTOs/MessageDTO.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.DTOs
+{
+    public class MessageDTO
+    {
+        public int Id { get; set; }
+        public string Content { get; set; }
+        public DateTime DateSent { get; set; }
+        public int SenderId { get; set; }
+        public int RecipientId { get; set; }
+    }
+}
