256dd71ee8de639ecafc39a2ffde82c9a2f245da
Админ <@DESKTOP-8DFR7S5>
2023-03-13 17:48:08 +0200

    Add Follow mapper

--- BusinessLogic/MapperProfiles/ApplicationProfile.cs
+++ BusinessLogic/MapperProfiles/ApplicationProfile.cs
@@ -19,6 +19,8 @@ namespace Core.MapperProfiles
             CreateMap<Like, LikeDTO>().ReverseMap();
 
             CreateMap<Message, MessageDTO>().ReverseMap();
+
+            CreateMap<Follow, FollowDTO>().ReverseMap();
         }
     }
 }
