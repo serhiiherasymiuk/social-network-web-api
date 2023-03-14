ae0430b8e0ceeefaccd46875d2d83f8a0b818bfa
Админ <@DESKTOP-8DFR7S5>
2023-03-14 16:48:56 +0200

    Remove User DTO, service, controller

--- BusinessLogic/Interfaces/IUsersService.cs
+++ BusinessLogic/Interfaces/IUsersService.cs
@@ -5,10 +5,6 @@ namespace Core.Interfaces
 {
     public interface IUsersService
     {
-        Task<IEnumerable<UserDTO>> GetAll();
-        Task<UserDTO?> GetById(int id);
-        Task Create(UserDTO user);
-        Task Edit(UserDTO user);
-        Task Delete(int id);
+
     }
 }
