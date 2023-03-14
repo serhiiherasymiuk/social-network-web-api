9fbbd6f08997d17caaefc95e19ca41f870274183
Админ <@DESKTOP-8DFR7S5>
2023-03-14 16:55:40 +0200

    Change IUserService

--- BusinessLogic/Interfaces/IUsersService.cs
+++ BusinessLogic/Interfaces/IUsersService.cs
@@ -1,10 +1,14 @@
 ﻿using Core.DTOs;
 using Core.Entities;
+using Microsoft.AspNetCore.Identity;
 
 namespace Core.Interfaces
 {
     public interface IUsersService
     {
-
+        Task<IdentityUser> Get(string id);
+        Task Login(LoginDTO loginDTO);
+        Task Register(RegisterDTO registerDTO);
+        Task Logout();
     }
 }
