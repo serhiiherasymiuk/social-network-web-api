ef70f1ef4de09364d2c04f6c102ed9b5b2005a6a
Админ <@DESKTOP-8DFR7S5>
2023-03-15 20:29:49 +0200

    Fix UserIdentity

--- BusinessLogic/Interfaces/IUsersService.cs
+++ BusinessLogic/Interfaces/IUsersService.cs
@@ -6,7 +6,7 @@ namespace Core.Interfaces
 {
     public interface IUsersService
     {
-        Task<IdentityUser> GetById(string id);
+        Task<User> GetById(string id);
         Task Login(LoginDTO loginDTO);
         Task Register(RegisterDTO registerDTO);
         Task Logout();
