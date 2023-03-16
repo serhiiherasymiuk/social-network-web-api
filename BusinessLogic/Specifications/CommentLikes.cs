34627cdade9147064c709ae4cfc1fc778142284a
Админ <@DESKTOP-8DFR7S5>
2023-03-16 14:29:57 +0200

    Add CommentLikes specifications

---
+++ BusinessLogic/Specifications/CommentLikes.cs
@@ -0,0 +1,39 @@
+﻿using Ardalis.Specification;
+using Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static Microsoft.EntityFrameworkCore.DbLoggerCategory;
+
+namespace Core.Specifications
+{
+    public static class CommentLikes
+    {
+        public class ById : Specification<CommentLike>
+        {
+            public ById(int id)
+            {
+                Query
+                    .Where(c => c.Id == id);
+            }
+        }
+        public class ByUserId : Specification<CommentLike>
+        {
+            public ByUserId(string userId)
+            {
+                Query
+                    .Where(c => c.UserId == userId);
+            }
+        }
+        public class ByCommentId : Specification<CommentLike>
+        {
+            public ByCommentId(int commentId)
+            {
+                Query
+                    .Where(c => c.CommentId == commentId);
+            }
+        }
+    }
+}
