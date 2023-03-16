49478a958f4a960083bfa6c256775a8bac2fd3df
Админ <@DESKTOP-8DFR7S5>
2023-03-16 18:49:30 +0200

    Add INotificationService

---
+++ BusinessLogic/Interfaces/INotificationService.cs
@@ -0,0 +1,19 @@
+﻿using Core.DTOs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Interfaces
+{
+    public interface INotificationService
+    {
+        Task<IEnumerable<NotificationDTO>> GetAll();
+        Task<NotificationDTO?> GetById(int id);
+        Task<IEnumerable<NotificationDTO>> GetByUserId(string userId);
+        Task Create(NotificationDTO notification);
+        Task Edit(NotificationDTO notification);
+        Task Delete(int id);
+    }
+}
