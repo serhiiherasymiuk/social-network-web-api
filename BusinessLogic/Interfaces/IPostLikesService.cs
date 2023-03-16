48d10669b605a190729fa1aad691a98a55ffe4ed
Админ <@DESKTOP-8DFR7S5>
2023-03-16 12:53:25 +0200

    Add IPostLikesService

---
+++ BusinessLogic/Interfaces/IPostLikesService.cs
@@ -0,0 +1,19 @@
+﻿using Core.DTOs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Interfaces
+{
+    public interface IPostLikesService
+    {
+        Task<IEnumerable<PostLikeDTO>> GetAll();
+        Task<IEnumerable<PostLikeDTO>> GetByUserId(string userId);
+        Task<IEnumerable<PostLikeDTO>> GetByPostId(string userId);
+        Task Create(PostLikeDTO post);
+        Task Edit(PostLikeDTO post);
+        Task Delete(int id);
+    }
+}
