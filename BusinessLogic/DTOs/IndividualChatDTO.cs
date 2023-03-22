2971e666dffc7b51f3684ba4d22d35acea18eb17
Админ <@DESKTOP-8DFR7S5>
2023-03-23 00:31:33 +0200

    Add IndividualChatDTO

---
+++ BusinessLogic/DTOs/IndividualChatDTO.cs
@@ -0,0 +1,10 @@
+﻿namespace Core.DTOs
+{
+    public class IndividualChatDTO
+    {
+        public string Id { get; set; }
+        public string User1Id { get; set; }
+        public string User2Id { get; set; }
+        public ICollection<IndividualChatMessageDTO> Messages { get; set; }
+    }
+}
