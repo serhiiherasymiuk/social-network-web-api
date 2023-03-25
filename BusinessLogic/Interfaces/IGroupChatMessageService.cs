b0ac2206dd1d60975ebba0db2cfd4a8686c42350
Админ <@DESKTOP-8DFR7S5>
2023-03-25 14:39:23 +0200

    Add IGroupChatMessageService interface

---
+++ BusinessLogic/Interfaces/IGroupChatMessageService.cs
@@ -0,0 +1,20 @@
+﻿using Core.DTOs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Interfaces
+{
+    public interface IGroupChatMessageService
+    {
+        Task<IEnumerable<GroupChatMessageDTO>> GetAll();
+        Task<GroupChatMessageDTO?> GetById(int id);
+        Task<IEnumerable<GroupChatMessageDTO>> GetByGroupChatId(int groupChatId);
+        Task<IEnumerable<GroupChatMessageDTO>> GetBySenderId(int userId);
+        Task Create(GroupChatMessageDTO groupChatMessage);
+        Task Edit(GroupChatMessageDTO groupChatMessage);
+        Task Delete(int id);
+    }
+}
